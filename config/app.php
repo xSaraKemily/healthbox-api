a7b62d9cef32ae96d86d62631a3e6babc368ef26
SaraKkemily <sarakemili@hotmail.com>
2022-03-27 23:00:49 -0300

    Implementado log-viewer no projeto

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -177,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Arcanedev\LogViewer\LogViewerServiceProvider::class
+
 
     ],
 
