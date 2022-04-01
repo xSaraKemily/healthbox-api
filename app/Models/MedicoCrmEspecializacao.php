1e3a8e1c3ede750e4f29fee63fef32cc959dbcaf
SaraKemily <sara@minertecnologia.com>
2022-04-01 10:48:54 -0300

    adicionado rules no model de crm

--- app/Models/MedicoCrmEspecializacao.php
+++ app/Models/MedicoCrmEspecializacao.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Requests\MedicoCrmEspecializacaoRequest;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,6 +18,24 @@ class MedicoCrmEspecializacao extends Model
         'medico_crm_id',
         'especializacao_id',
     ];
+
+    public function rules()
+    {
+        return [
+            'medico_crm_id' => [
+                'required',
+                'exists:medicos_crm,id',
+            ],
+            'especializacao_id' => [
+                'required',
+                'exists:especializacoes,id',
+                Rule::unique('medico_crm_especializacoes')->where(function ($query) {
+                    return $query->where('medico_crm_id', $this->medico_crm_id)
+                        ->where('especializacao_id', $this->especializacao_id);
+                })->ignore($this->id),
+            ]
+        ];
+    }
 
     public function crm()
     {
