2b04c9c39b7ec587d3297473d0408afe44be6614
SaraKemily <sara@minertecnologia.com>
2022-04-29 07:22:41 -0300

    Criado colunas de comorbidades e alergias no cadastro de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,8 @@ class User extends Authenticatable  implements JWTSubject
         'sexo',
         'telefone',
         'foto_path',
+        'comorbidades',
+        'alergias',
         'ativo',
     ];
 
