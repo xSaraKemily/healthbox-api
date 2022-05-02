940174f6d6a165c91b11234c9afea447a54b7e4e
SaraKemily <sara@minertecnologia.com>
2022-05-02 13:16:59 -0300

    criado rota para deletar acompanhamentos

--- routes/api.php
+++ routes/api.php
@@ -77,6 +77,7 @@ Route::group(['middleware' => 'auth:api',], function () {
 
     Route::post('acompanhamentos', [AcompanhamentoController::class, 'store']);
     Route::put('acompanhamentos/{id}', [AcompanhamentoController::class, 'update']);
+    Route::delete('acompanhamentos/{id}', [AcompanhamentoController::class, 'destroy']);
     Route::get('acompanhamentos/vinculos-usuarios', [AcompanhamentoController::class, 'usuarioVinculo']);
     Route::get('acompanhamentos', [AcompanhamentoController::class, 'index']);
 
