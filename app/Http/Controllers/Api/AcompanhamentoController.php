416517427197696d78c0c0dd0ad26f541f051fb0
SaraKemily <sara@minertecnologia.com>
2022-05-04 10:03:30 -0300

    debug

--- app/Http/Controllers/Api/AcompanhamentoController.php
+++ app/Http/Controllers/Api/AcompanhamentoController.php
@@ -113,7 +113,7 @@ class AcompanhamentoController extends Controller
         }
 
         $acompanhamentos = $acompanhamentos->get();
-
+dd($acompanhamentos);
         $questionarios = [];
         foreach ($acompanhamentos as $acompanhamento) {
             $dataAnterior     = Carbon::parse($acompanhamento->data_inicio);
@@ -137,7 +137,7 @@ class AcompanhamentoController extends Controller
             }
 
 //            $dataAtual = Carbon::now()->format('Y-m-d');
-dd($datasRespostas);
+
             foreach ($datasRespostas as $data) {
                 $pendente = true;
 
